fd636b56e654fe336e433bc18d7c3d28d6168e68
Your Name <zuhaib@mvp-apps.ae>
2025-03-13 15:16:26 +0400

    no code changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,45 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://zuhaib10.github.io',
-  base: 'portfolio',
+    output: 'static',
+    trailingSlash: 'always',
+    site: 'https://zuhaib10.github.io',
+    base: 'portfolio',
+
+    // Single page, no prefetch needed
+    prefetch: false,
+
+    integrations: [
+        tailwind(),
+        sitemap(),
+        compress({
+            CSS: true,
+            SVG: false,
+            Image: false,
+            HTML: {
+                "html-minifier-terser": {
+                    collapseWhitespace: true,
+                    // collapseInlineTagWhitespace: true, // It breaks display-inline / flex-inline text
+                    minifyCSS: true,
+                    minifyJS: true,
+                    removeComments: true,
+                    removeEmptyAttributes: true,
+                    // removeEmptyElements: true, // It removes sometimes SVGs
+                    removeRedundantAttributes: true
+                },
+            },
+            JavaScript: {
+                'terser': {
+                    compress: {
+                        drop_console: true,
+                        drop_debugger: true,
+                    }
+                }
+            }
+        })
+    ]
 });
