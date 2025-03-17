0d3407273c5ac1ac34251e195c0195b33df293d1
Your Name <zuhaib@mvp-apps.ae>
2025-03-17 15:45:02 +0400

    Add .gitignore, remove unused images, and introduce context cursor types

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,29 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import astroIcon from 'astro-icon';
+import mdx from '@astrojs/mdx';
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://zuhaib10.github.io',
     base: 'portfolio',
+    integrations: [
+        tailwind(),
+        mdx(),
+        astroIcon({
+            include: {
+                mdi: ["*"],
+                ri: ['*'],
+                'simple-icons': ['*'],
+            },
+        }),
+        playformCompress({
+            CSS: false,
+            Image: false,
+            Action: {
+                Passed: async () => true,   // https://github.com/PlayForm/Compress/issues/376
+            },
+        })
+    ],
 });
